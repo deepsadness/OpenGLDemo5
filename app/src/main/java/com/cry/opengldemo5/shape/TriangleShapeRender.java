653418e2fd3ccc4ffc033671ed8521ffd61373e0
钟朝旭Frank_Android <zhong.zhaoxu@hbtown.com>
2018-05-04 10:21:31 +0800

    添加三角形render。完成

--- app/src/main/java/com/cry/opengldemo5/shape/TriangleShapeRender.java
+++ app/src/main/java/com/cry/opengldemo5/shape/TriangleShapeRender.java
@@ -38,30 +38,55 @@ public class TriangleShapeRender extends ViewGLRender {
 
     //在数组中，一个顶点需要3个来描述其位置，需要3个偏移量
     private static final int COORDS_PER_VERTEX = 3;
-    //在数组中，代表vertex的点的数量
-    private static final int POSITION_COMPONENT_COUNT = 3;
+    private static final int COORDS_PER_COLOR = 0;
+
     //在数组中，描述一个顶点，总共的顶点需要的偏移量。这里因为只有位置顶点，所以和上面的值一样
-    private static final int TOTAL_COMPONENT_COUNT = COORDS_PER_VERTEX;
+    private static final int TOTAL_COMPONENT_COUNT = COORDS_PER_VERTEX+COORDS_PER_COLOR;
     //一个点需要的byte偏移量。
     private static final int STRIDE = TOTAL_COMPONENT_COUNT * Constant.BYTES_PER_FLOAT;
 
     //顶点的坐标系
     private static float TRIANGLE_COORDS[] = {
-            //Order of coordinates: X, Y, Z
+            //Order of coordinates: X, Y, Z，RGB
             0.5f, 0.5f, 0.0f, // top
             -0.5f, -0.5f, 0.0f, // bottom left
-            0.5f, -0.5f, 0.0f  // bottom right
+            0.5f, -0.5f, 0.0f   // bottom right
     };
-    //设置颜色，依次为红绿蓝和透明通道
+    //设置颜色，依次为红绿蓝和透明通道。
+    //因为颜色是常量，所以用单独的数据表示？
     private static float TRIANGLE_COLOR[] = {1.0f, 1.0f, 1.0f, 1.0f};
-    private static final int VERTEX_COUNT = TRIANGLE_COORDS.length /COORDS_PER_VERTEX;
+    private static final int VERTEX_COUNT = TRIANGLE_COORDS.length / TOTAL_COMPONENT_COUNT;
+    private final Context context;
 
     //pragram的指针
-    private final int mProgramObjectId;
+    private int mProgramObjectId;
     //顶点数据的内存映射
     private final FloatBuffer mVertexFloatBuffer;
-    public TriangleShapeRender(Context context) {
 
+    public TriangleShapeRender(Context context) {
+        this.context = context;
+  /*
+        0. 调用GLES20的包的方法时，其实就是调用JNI的方法。
+        所以分配本地的内存块，将java数据复制到本地内存中，而本地内存可以不受垃圾回收的控制
+        1. 使用nio中的ByteBuffer来创建内存区域。
+        2. ByteOrder.nativeOrder()来保证，同一个平台使用相同的顺序
+        3. 然后可以通过put方法，将内存复制过去。
+
+        因为这里是Float，所以就使用floatBuffer
+         */
+        mVertexFloatBuffer = ByteBuffer
+                .allocateDirect(TRIANGLE_COORDS.length * Constant.BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(TRIANGLE_COORDS);
+        mVertexFloatBuffer.position(0);
+
+
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        super.onSurfaceCreated(gl, config);
         //0.先去得到着色器的代码
         String vertexShaderCode = GLESUtils.readAssetShaderCode(context, VERTEX_SHADER_FILE);
         String fragmentShaderCode = GLESUtils.readAssetShaderCode(context, FRAGMENT_SHADER_FILE);
@@ -76,37 +101,17 @@ public class TriangleShapeRender extends ViewGLRender {
         GLES20.glAttachShader(mProgramObjectId, fragmentShaderObjectId);
         //4.最后，启动GL link program
         GLES20.glLinkProgram(mProgramObjectId);
-
-
-        /*
-        0. 调用GLES20的包的方法时，其实就是调用JNI的方法。
-        所以分配本地的内存块，将java数据复制到本地内存中，而本地内存可以不受垃圾回收的控制
-        1. 使用nio中的ByteBuffer来创建内存区域。
-        2. ByteOrder.nativeOrder()来保证，同一个平台使用相同的顺序
-        3. 然后可以通过put方法，将内存复制过去。
-
-        因为这里是Float，所以就使用floatBuffer
-         */
-        mVertexFloatBuffer = ByteBuffer
-                .allocateDirect(TRIANGLE_COORDS.length * Constant.BYTES_PER_FLOAT)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer()
-                .put(TRIANGLE_COORDS);
-
-    }
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-
+        super.onSurfaceChanged(gl, width, height);
     }
+
     //在OnDrawFrame中进行绘制
     @Override
     public void onDrawFrame(GL10 gl) {
-//        super.onDrawFrame(gl);
+        super.onDrawFrame(gl);
 
         //0.先使用这个program?这一步应该可以放到onCreate中进行
         GLES20.glUseProgram(mProgramObjectId);
@@ -118,7 +123,7 @@ public class TriangleShapeRender extends ViewGLRender {
         //3.将坐标数据放入
         GLES20.glVertexAttribPointer(
                 vPosition,  //上面得到的id
-                COORDS_PER_VERTEX , //告诉他用几个便宜量来描述一个顶点
+                COORDS_PER_VERTEX, //告诉他用几个偏移量来描述一个顶点
                 GLES20.GL_FLOAT, false,
                 STRIDE, //一个顶点需要多少个字节的偏移量
                 mVertexFloatBuffer);
@@ -133,9 +138,10 @@ public class TriangleShapeRender extends ViewGLRender {
                 1,
                 TRIANGLE_COLOR,
                 0
-                );
+        );
 
         //绘制三角形.
+        //draw arrays的几种方式 GL_TRIANGLES三角形 GL_TRIANGLE_STRIP三角形带的方式(开始的3个点描述一个三角形，后面每多一个点，多一个三角形) GL_TRIANGLE_FAN扇形(可以描述圆形)
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, VERTEX_COUNT);
         //禁止顶点数组的句柄
         GLES20.glDisableVertexAttribArray(vPosition);
