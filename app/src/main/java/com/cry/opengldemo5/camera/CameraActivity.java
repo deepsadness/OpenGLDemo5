eaf1493940db1f4314575076ce4a3385479df2ea
钟朝旭Frank_Android <zhong.zhaoxu@hbtown.com>
2018-05-10 09:53:48 +0800

    添加权限

--- app/src/main/java/com/cry/opengldemo5/camera/CameraActivity.java
+++ app/src/main/java/com/cry/opengldemo5/camera/CameraActivity.java
@@ -1,13 +1,24 @@
 package com.cry.opengldemo5.camera;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.cry.opengldemo5.camera.gles.CameraView;
 
 public class CameraActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
     public CameraView mCameraView;
 
     @Override
@@ -18,19 +29,119 @@ public class CameraActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        mCameraView = new CameraView(this);
-        setContentView(mCameraView);
+
     }
+
 
     @Override
     protected void onPause() {
         super.onPause();
-        mCameraView.onPause();
+        if (mCameraView != null) {
+            mCameraView.onPause();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mCameraView.onResume();
+        if (mCameraView != null) {
+            mCameraView.onResume();
+        } else {
+            requestPermission();
+        }
+    }
+
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "已经获取权限", Toast.LENGTH_SHORT).show();
+            startCamera();
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            ConfirmationDialogFragment
+                    .newInstance("camera_permission_confirmation",
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_CAMERA_PERMISSION,
+                            "camera_permission_not_granted")
+                    .show(getSupportFragmentManager(), "FRAGMENT_DIALOG");
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA_PERMISSION);
+        }
+    }
+
+    private void startCamera() {
+        if (mCameraView == null) {
+            mCameraView = new CameraView(this);
+            setContentView(mCameraView);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION:
+                if (permissions.length != 1 || grantResults.length != 1) {
+                    throw new RuntimeException("Error on requesting camera permission.");
+                }
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "camera_permission_not_granted",
+                            Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                // No need to start camera here; it is handled by onResume
+                startCamera();
+                break;
+        }
+    }
+
+    public static class ConfirmationDialogFragment extends DialogFragment {
+
+        private static final String ARG_MESSAGE = "message";
+        private static final String ARG_PERMISSIONS = "permissions";
+        private static final String ARG_REQUEST_CODE = "request_code";
+        private static final String ARG_NOT_GRANTED_MESSAGE = "not_granted_message";
+
+        public static ConfirmationDialogFragment newInstance(String message,
+                                                             String[] permissions, int requestCode, String notGrantedMessage) {
+            ConfirmationDialogFragment fragment = new ConfirmationDialogFragment();
+            Bundle args = new Bundle();
+            args.putString(ARG_MESSAGE, message);
+            args.putStringArray(ARG_PERMISSIONS, permissions);
+            args.putInt(ARG_REQUEST_CODE, requestCode);
+            args.putString(ARG_NOT_GRANTED_MESSAGE, notGrantedMessage);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Bundle args = getArguments();
+            return new AlertDialog.Builder(getActivity())
+                    .setMessage(args.getString(ARG_MESSAGE, ""))
+                    .setPositiveButton(android.R.string.ok,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    String[] permissions = args.getStringArray(ARG_PERMISSIONS);
+                                    if (permissions == null) {
+                                        throw new IllegalArgumentException();
+                                    }
+                                    ActivityCompat.requestPermissions(getActivity(),
+                                            permissions, args.getInt(ARG_REQUEST_CODE));
+                                }
+                            })
+                    .setNegativeButton(android.R.string.cancel,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(getActivity(),
+                                            args.getString(ARG_NOT_GRANTED_MESSAGE),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                    .create();
+        }
+
     }
 }
