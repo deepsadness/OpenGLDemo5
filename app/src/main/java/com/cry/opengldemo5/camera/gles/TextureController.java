67f90efe6de4c7b5cec01be2a3d229ca4f712c83
deepsadness <a295756092@gmail.com>
2018-05-12 00:10:52 +0800

    添加注释

--- app/src/main/java/com/cry/opengldemo5/camera/gles/TextureController.java
+++ app/src/main/java/com/cry/opengldemo5/camera/gles/TextureController.java
@@ -2,6 +2,7 @@ package com.cry.opengldemo5.camera.gles;
 
 import android.content.Context;
 import android.graphics.SurfaceTexture;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,11 @@ import com.cry.opengldemo5.camera.Camera2Activity;
 import com.cry.opengldemo5.camera.core.ISize;
 import com.cry.opengldemo5.camera.gles.filter.AFilter;
 import com.cry.opengldemo5.camera.gles.filter.GroupFilter;
+import com.cry.opengldemo5.camera.gles.filter.NoFilter;
 import com.cry.opengldemo5.camera.gles.filter.TextureFilter;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
@@ -20,6 +25,7 @@ import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Texture filter controller.
+ * 将自己的GLView的生命周期监听到。分发给其他的render
  * <p>
  * Created by a2957 on 2018/5/10.
  */
@@ -30,16 +36,46 @@ public class TextureController implements GLSurfaceView.Renderer {
     private ISize previewSize;
     private ISize picSize;
 
-    //    private Renderer mRenderer;                                 //用户附加的Renderer或用来监听Renderer
+    //    private Renderer mRenderer;
+    //主要就是下面这个三个Render了.再仔细来看一下这三个Render
     private TextureFilter mEffectFilter;                        //特效处理的Filter
     private GroupFilter mGroupFilter;                           //中间特效
-    private AFilter mShowFilter;
+    private AFilter mShowFilter;                                //用来渲染输出的Filter
     private Object mNativeWindowSurface;
-    private Camera2Activity.Camera1Render mRender;
+    private Camera2Activity.Camera1Render mRender;                //用户附加的Renderer或用来监听Renderer
+
+    //离屏的buffer.最后用来导出数据
+    private int[] mExportFrame = new int[1];
+    private int[] mExportTexture = new int[1];
+
+    private ByteBuffer[] outputBuffer = new ByteBuffer[3]; //用于存储回调数据的buffer
+
+    private int indexOutput = 0; //回调数据的buffer
+    private AtomicBoolean isParamSet = new AtomicBoolean(false);
+
+    //matrix
+    private float[] SM=new float[16];                           //用于绘制到屏幕上的变换矩阵
+    private float[] callbackOM=new float[16];                   //用于绘制回调缩放的矩阵
+
 
     public TextureController(Context context) {
         this.mContext = context;
         init(context);
+    }
+
+    public void surfaceCreated(Object nativeWindow) {
+        this.mNativeWindowSurface = nativeWindow;
+        //调用GLView的生命周期方法？
+        mGLView.surfaceCreated(null);
+    }
+
+    public void surfaceChanged(int width, int height) {
+        this.previewSize = new ISize(width, height);
+        mGLView.surfaceChanged(null, 0, width, height);
+    }
+
+    public void surfaceDestroyed() {
+        mGLView.surfaceDestroyed(null);
     }
 
     private void init(Context context) {
@@ -55,6 +91,9 @@ public class TextureController implements GLSurfaceView.Renderer {
         v.addView(mGLView);
         v.setVisibility(View.GONE);
         //开始设置滤镜。因为经过多次滤镜。所以我们需要创建多个滤镜
+        mEffectFilter = new TextureFilter(mContext.getResources());
+        mShowFilter = new NoFilter(mContext.getResources());
+        mGroupFilter = new GroupFilter(mContext.getResources());
 
         //设置Camera中提供的previewSize 和实际的picSize
 
@@ -62,20 +101,97 @@ public class TextureController implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        //创建各种Render
+        mEffectFilter.create();
+        mGroupFilter.create();
+        mShowFilter.create();
+        if (!isParamSet.get()) {
+            if (mRender != null) {
+                mRender.onSurfaceCreated(gl, config);
+                picSize = mRender.mPictureSize;
+            }
+            sdkParamSet();
+        }
+        calculateCallbackOM();
 
+        //先deleteFrameBuffer。然后重新创建
+        deleteFrameBuffer();
+        //重新创建
+        GLES20.glGenFramebuffers(1, mExportFrame, 0);
+        GLES20.glGenTextures(1, mExportTexture, 0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mExportTexture[0]);
+        GLES20.glTexImage2D(
+                GLES20.GL_TEXTURE_2D, 0,
+                GLES20.GL_RGBA,
+                picSize.getWidth(), picSize.getHeight(), 0,
+                GLES20.GL_RGBA,
+                GLES20.GL_UNSIGNED_BYTE, null);
 
+        //设置缩小过滤为使用纹理中坐标最接近的一个像素的颜色作为需要绘制的像素颜色
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+        //设置放大过滤为使用纹理中坐标最接近的若干个颜色，通过加权平均算法得到需要绘制的像素颜色
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        //设置环绕方向S，截取纹理坐标到[1/2n,1-1/2n]。将导致永远不会与border融合
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        //设置环绕方向T，截取纹理坐标到[1/2n,1-1/2n]。将导致永远不会与border融合
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+
+    }
+
+    /*
+    删除的是FrameBuffer和Texture的纹理
+     */
+    private void deleteFrameBuffer() {
+        GLES20.glDeleteFramebuffers(1, mExportFrame, 0);
+        GLES20.glDeleteTextures(1, mExportTexture, 0);
+    }
+
+    private void sdkParamSet() {
+        if (!isParamSet.get() && picSize.getWidth() > 0 && picSize.getHeight() > 0) {
+            isParamSet.set(true);
+        }
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-
+        mShowFilter.setSize(width, height);
+        mShowFilter.setMatrix(SM);
+        mGroupFilter.setSize(picSize.getWidth(),picSize.getHeight());
+        mEffectFilter.setSize(picSize.getWidth(),picSize.getHeight());
+        mShowFilter.setSize(picSize.getWidth(),picSize.getHeight());
+        if(mRender!=null){
+            mRender.onSurfaceChanged(gl, width, height);
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
+        if(isParamSet.get()){
+            mEffectFilter.draw();
+            mGroupFilter.setTextureId(mEffectFilter.getOutputTexture());
+            mGroupFilter.draw();
 
+            //显示传入的texture上，一般是显示在屏幕上
+            GLES20.glViewport(0,0,previewSize.getWidth(),previewSize.getHeight());
+            mShowFilter.setMatrix(SM);
+            mShowFilter.setTextureId(mGroupFilter.getOutputTexture());
+            mShowFilter.draw();
+            if(mRender!=null){
+                mRender.onDrawFrame(gl);
+            }
+
+//            callbackIfNeeded();
+        }
     }
-
+    private void calculateCallbackOM(){
+//        if(frameCallbackHeight>0&&frameCallbackWidth>0&&picSize.getWidth()>0&&picSize.getHeight()>0){
+//            //计算输出的变换矩阵
+//            MatrixUtils.getMatrix(callbackOM,MatrixUtils.TYPE_CENTERCROP,picSize.getWidth(), picSize.getHeight(),
+//                    frameCallbackWidth,
+//                    frameCallbackHeight);
+//            MatrixUtils.flip(callbackOM,false,true);
+//        }
+    }
     public void setPreviewSize(ISize previewSize) {
         this.previewSize = previewSize;
     }
@@ -92,24 +208,19 @@ public class TextureController implements GLSurfaceView.Renderer {
         mGLView.requestRender();
     }
 
-    public void surfaceCreated(Object nativeWindow) {
-        this.mNativeWindowSurface = nativeWindow;
-        //调用GLView的生命周期方法？
-        mGLView.surfaceCreated(null);
+    /**
+     * 增加滤镜
+     * @param filter 滤镜
+     */
+    public void addFilter(AFilter filter){
+        mGroupFilter.addFilter(filter);
     }
+
 
     public void setRender(Camera2Activity.Camera1Render render) {
         mRender = render;
-        picSize = mRender.mPictureSize;
-    }
-
-    public void surfaceChanged(int width, int height) {
-        this.previewSize = new ISize(width, height);
-        mGLView.surfaceChanged(null, 0, width, height);
-    }
-
-    public void surfaceDestroyed() {
-        mGLView.surfaceDestroyed(null);
+        //这个时候不能设置，因为都还没有走到SurfaceCreated方法中
+//        picSize = mRender.mPictureSize;
     }
 
     public void onResume() {
@@ -154,12 +265,13 @@ public class TextureController implements GLSurfaceView.Renderer {
             setEGLWindowSurfaceFactory(new EGLWindowSurfaceFactory() {
                 @Override
                 public EGLSurface createWindowSurface(EGL10 egl, EGLDisplay display, EGLConfig config, Object nativeWindow) {
-                    return null;
+                    //在这里传入了我们的surface
+                    return egl.eglCreateWindowSurface(display, config, mNativeWindowSurface, null);
                 }
 
                 @Override
                 public void destroySurface(EGL10 egl, EGLDisplay display, EGLSurface surface) {
-
+                    egl.eglDestroySurface(display, surface);
                 }
             });
             setEGLContextClientVersion(2);
--- app/src/main/java/com/cry/opengldemo5/camera/gles/filter/TextureFilter.java
+++ app/src/main/java/com/cry/opengldemo5/camera/gles/filter/TextureFilter.java
@@ -19,6 +19,17 @@ import java.nio.ByteBuffer;
 
 /**
  * Description:
+ *
+ * 这个TextureFilter 是一个特效处理的Filter?
+ * Filter其实就是在Render的对应生命周期进行绘制，变形的Render类的抽象
+ *
+ * TextureFilter里面有一个CameraFilter 来讲相机传递的参数画出来 oes 纹理
+ *
+ * 父类默认是画全屏
+ *
+ * 主要就是完成这一步转换的滤镜。为什么不和后面的其他滤镜组合在一起？我的理解是有部分原因是因为这里导入的是oes的问题，到frameBuffer
+ * 后面的滤镜都不需要导入oes的纹理，所以。。。。
+ *
  */
 public class TextureFilter extends AFilter {
 
@@ -26,11 +37,14 @@ public class TextureFilter extends AFilter {
     private int width = 0;
     private int height = 0;
 
+    //离屏的buffer?
     private int[] fFrame = new int[1];
     private int[] fTexture = new int[1];
+    //相机的Render中，创建的textureId 代表oes纹理的name
     private int[] mCameraTexture = new int[1];
-
+    //创建的SurfaceTextureView 传递给相机。来作为输入口
     private SurfaceTexture mSurfaceTexture;
+    //坐标系
     private float[] mCoordOM = new float[16];
 
     //获取Track数据
@@ -45,6 +59,7 @@ public class TextureFilter extends AFilter {
         mFilter.setCoordMatrix(matrix);
     }
 
+    //通过这里get方法，和外面相机的setPreview方法联通。作为输入口
     public SurfaceTexture getTexture() {
         return mSurfaceTexture;
     }
@@ -54,6 +69,7 @@ public class TextureFilter extends AFilter {
         mFilter.setFlag(flag);
     }
 
+    //本身不需要绘制
     @Override
     protected void initBuffer() {
 
@@ -64,29 +80,46 @@ public class TextureFilter extends AFilter {
         mFilter.setMatrix(matrix);
     }
 
+    //得到向外输出的textureName
+    //数据从相机输入到cameraTextureId,然后我们再输入到frameBuffer的textureid
     @Override
     public int getOutputTexture() {
         return fTexture[0];
     }
 
+    //绘制。就是添加特效的过程。
+    //这里需要先调用SurfaceView updateImage方法。因为是接受相机的回调。
+
     @Override
     public void draw() {
+        //关掉深度检查？
         boolean a = GLES20.glIsEnabled(GLES20.GL_DEPTH_TEST);
         if (a) {
             GLES20.glDisable(GLES20.GL_DEPTH_TEST);
         }
+        //告诉surfaceTextureView更新了。
         if (mSurfaceTexture != null) {
             mSurfaceTexture.updateTexImage();
             mSurfaceTexture.getTransformMatrix(mCoordOM);
             mFilter.setCoordMatrix(mCoordOM);
         }
-        EasyGlUtils.bindFrameTexture(fFrame[0], fTexture[0]);
+
+        //使用Color Attachment的方式，将绑定的FrameBuffer 绑定到fTextureId上
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, fFrame[0]);
+        GLES20.glFramebufferTexture2D(GLES20.GL_FRAMEBUFFER, GLES20.GL_COLOR_ATTACHMENT0,
+                GLES20.GL_TEXTURE_2D, fTexture[0], 0);
+        //设置视图的大小？
         GLES20.glViewport(0, 0, width, height);
         mFilter.setTextureId(mCameraTexture[0]);
+        //调用Camera的Filter 进行draw就是想相机得到的数据先画出来
+        //因为绑定了FrameBuffer,所有数据就到frameBuffer上来？这样我们就可以根据ftexture[0]拿到这部分的数据了？
+
         mFilter.draw();
         Log.e("wuwang", "textureFilter draw");
-        EasyGlUtils.unBindFrameBuffer();
+        //调用完了，就解绑FrameBuffer的绑定
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER,0);
 
+        //恢复深度检测。不影响下一个路径。
         if (a) {
             GLES20.glEnable(GLES20.GL_DEPTH_TEST);
         }
